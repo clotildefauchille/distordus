d6c3d7bb5e6c782f62723011dbb6fb6f09786aa9
CLOTILDE FAUCHILLE <clotildefauchille@macbook-pro-de-clotilde.home>
2020-06-09 15:43:47 +0200

    create element from js

--- scripts.js
+++ scripts.js
@@ -1,9 +1,42 @@
-document.getElementById("record")
-let recordButton=document.getElementById("record")
-function myFunction() {
-    recordButton.style.backgroundColor = "red";
+let recordButton = document.getElementById("record")
+var buttonsElement = document.getElementById('buttons')
+var headerElement = document.getElementById("Welcome")
+function changeButtonColor(button, backgroundColor, textColor) {
+    button.style.backgroundColor = backgroundColor
+    button.style.color = textColor
+    return "poute"
+}
+function redButtonRecord() {
+    const result = changeButtonColor(recordButton, "red", "white")
+    console.log("result: ", result)
 }
 function stopRecord() {
-    recordButton.style.backgroundColor = "transparent"
-    alert ("ne pas oublier d'afficher un formulaire")
+    changeButtonColor(recordButton, "transparent", "black")
 }
+function createElementAudioDisplay() {
+    var audioElement = document.createElement('audio')
+    audioElement.setAttribute("controls", true)
+    document.body.insertBefore(audioElement, buttonsElement);
+}
+createElementAudioDisplay()
+function audioFileName() {
+    var newTitle= document.createTextNode("mon titre bip audio")
+    var textEdit = document.createElement("div")
+    textEdit.appendChild(newTitle)
+    document.body.insertBefore(textEdit, buttonsElement);
+}
+audioFileName()
+function IdidIt() {
+    var newPtitle= document.createTextNode("clotte assure")
+    var textEdit2 = document.createElement("p")
+    var prouteButton = document.createElement("button")
+    var textprouteButton = document.createTextNode ("proute");
+
+    textEdit2.appendChild(newPtitle)
+    textEdit2.appendChild(prouteButton)
+    prouteButton.appendChild(textprouteButton)
+    document.body.insertBefore(textEdit2, headerElement)
+    document.body.insertBefore(prouteButton, headerElement)
+}
+IdidIt()
+
