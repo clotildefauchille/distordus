e7869c5bdbc99317de28a5c0c65eadd36702e6e6
CLOTILDE FAUCHILLE <clotildefauchille@macbook-pro-de-clotilde.home>
2020-06-11 14:51:34 +0200

    button enable/disable

--- scripts.js
+++ scripts.js
@@ -1,19 +1,26 @@
 let recordButton = document.getElementById("record")
 var buttonsElement = document.getElementById('buttons')
 var headerElement = document.getElementById("Welcome")
+let stopButton = document.getElementById ("stop")
+
 function changeButtonColor(button, backgroundColor, textColor) {
     button.style.backgroundColor = backgroundColor
     button.style.color = textColor
     return "poute"
 }
-function redButtonRecord() {
+function startRecord() {
+    stopButton.disabled = false;
+    recordButton.disabled = true;
     const result = changeButtonColor(recordButton, "red", "white")
     console.log("result: ", result)
 }
 function stopRecord() {
     changeButtonColor(recordButton, "transparent", "black")
-    createElementAudioDisplay()
     audioFileName()
+    createElementAudioDisplay()
+    stopButton.disabled = true;
+    recordButton.disabled = false;
+    
 }
 function createElementAudioDisplay() {
     var audioElement = document.createElement('audio')
@@ -26,16 +33,4 @@ function audioFileName() {
     textEdit.appendChild(newTitle)
     document.body.insertBefore(textEdit, buttonsElement);
 }
-function IdidIt() {
-    var newPtitle= document.createTextNode("clotte assure")
-    var textEdit2 = document.createElement("p")
-    var prouteButton = document.createElement("button")
-    var textprouteButton = document.createTextNode ("proute");
 
-    textEdit2.appendChild(newPtitle)
-    textEdit2.appendChild(prouteButton)
-    prouteButton.appendChild(textprouteButton)
-    document.body.insertBefore(textEdit2, headerElement)
-    document.body.insertBefore(prouteButton, headerElement)
-}
-IdidIt()
