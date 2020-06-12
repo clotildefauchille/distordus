8febd0324a1735ca8a470d5fe0d709d3df168176
CLOTILDE FAUCHILLE <clotildefauchille@macbook-pro-de-clotilde.home>
2020-06-12 15:13:36 +0200

    wip first try mediarecorder

--- scripts.js
+++ scripts.js
@@ -1,36 +1,54 @@
 let recordButton = document.getElementById("record")
 var buttonsElement = document.getElementById('buttons')
 var headerElement = document.getElementById("Welcome")
-let stopButton = document.getElementById ("stop")
+let stopButton = document.getElementById("stop")
+let chunks = []
+let constraints = { audio: true }
+let audioCreateContainers = function (stream) {
+    const mediaRecorder = new MediaRecorder(stream)
+    recordButton.onclick = function () {
+        mediaRecorder.start()
+        stopButton.disabled = false;
+        recordButton.disabled = true;
+        const result = changeButtonColor(recordButton, "red", "white")
+        console.log("result: ", result)
+    }
+    stopButton.onclick = function () {
+        mediaRecorder.stop()
+        changeButtonColor(recordButton, "transparent", "black")
+        stopButton.disabled = true;
+        recordButton.disabled = false;
+
+    }
+    mediaRecorder.onstop = function (e) {
+        const blob = new Blob(chunks, { 'type': 'audio/ogg; codecs=opus' });
+        chunks = [];
+        const audioURL = window.URL.createObjectURL(blob);
+        createElementAudioDisplay(audioURL)
+        audioFileName()
+
+    }
+}
 
 function changeButtonColor(button, backgroundColor, textColor) {
     button.style.backgroundColor = backgroundColor
     button.style.color = textColor
     return "poute"
 }
-function startRecord() {
-    stopButton.disabled = false;
-    recordButton.disabled = true;
-    const result = changeButtonColor(recordButton, "red", "white")
-    console.log("result: ", result)
-}
-function stopRecord() {
-    changeButtonColor(recordButton, "transparent", "black")
-    audioFileName()
-    createElementAudioDisplay()
-    stopButton.disabled = true;
-    recordButton.disabled = false;
-    
-}
-function createElementAudioDisplay() {
+
+function createElementAudioDisplay(audioURL) {
     var audioElement = document.createElement('audio')
     audioElement.setAttribute("controls", true)
+    audioElement.src = audioURL
     document.body.insertBefore(audioElement, buttonsElement);
 }
 function audioFileName() {
-    var newTitle= document.createTextNode("mon titre bip audio")
+    var newTitle = document.createTextNode("mon titre bip audio")
     var textEdit = document.createElement("div")
     textEdit.appendChild(newTitle)
     document.body.insertBefore(textEdit, buttonsElement);
 }
-
+let onError = function (err) {
+    console.log('The following error occured: ' + err);
+}
+navigator.mediaDevices.getUserMedia(constraints).then(stream => audioCreateContainers(stream), onError)
