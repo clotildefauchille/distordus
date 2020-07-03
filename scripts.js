b478c09cd532b8c0eb473e606e7e0ce0e2460384
CLOTILDE FAUCHILLE <clotildefauchille@macbook-pro-de-clotilde.home>
2020-07-03 15:20:20 +0200

    change filter when change selection

--- scripts.js
+++ scripts.js
@@ -36,10 +36,23 @@ let audioCreateContainers = function (stream) {
 
     var distortion = audioCtx.createWaveShaper()
     var source = audioCtx.createMediaStreamSource(stream)
+    var biquadFilter = audioCtx.createBiquadFilter()
+    var biquadFilter2 = audioCtx.createBiquadFilter()
+    function connectHighPassFilter() {
+        source.connect(biquadFilter)
+        biquadFilter.type = "highpass";
+        biquadFilter.frequency.setValueAtTime(6000, audioCtx.currentTime);
+        biquadFilter.connect(biquadFilter2)
+        biquadFilter2.type = "peaking";
+        biquadFilter2.frequency.setValueAtTime(7000, audioCtx.currentTime);
+        biquadFilter2.Q = 20
+        biquadFilter2.gain.setValueAtTime(30, audioCtx.currentTime);
+        biquadFilter2.connect(streamDestination)
+    }
     function connectDistordusFilter() {
         source.connect(distortion);
         function makeDistortionCurve(amount) {
-            var k = typeof amount === 'number' ? amount : 30,
+            var k = typeof amount === 'number' ? amount : 60,
                 n_samples = 44100,
                 curve = new Float32Array(n_samples),
                 deg = Math.PI / 180,
@@ -57,10 +70,17 @@ let audioCreateContainers = function (stream) {
 
     }
     var elementThatIsNamedSelect = document.getElementById("audio-filters");
-    var audioFilterSelected = elementThatIsNamedSelect.options[elementThatIsNamedSelect.selectedIndex].value;
-    if (audioFilterSelected === "distordus") { connectDistordusFilter()}
+    connectDistordusFilter()
     elementThatIsNamedSelect.addEventListener('change', function () {
-        if (audioFilterSelected === "distordus") { connectDistordusFilter() }
+        var audioFilterSelected = elementThatIsNamedSelect.options[elementThatIsNamedSelect.selectedIndex].value;
+        if (audioFilterSelected === "distordus") {
+            biquadFilter2.disconnect()
+            connectDistordusFilter()
+        }
+        else if (audioFilterSelected === "HighPass") {
+            distortion.disconnect()
+            connectHighPassFilter()
+        }
     });
 }
 
@@ -78,7 +98,7 @@ function createElementAudioDisplay(audioURL) {
     document.body.insertBefore(audioElement, buttonsElement);
 }
 function audioFileName() {
-    var newTitle = document.createTextNode("mon titre bip audio")
+    var newTitle = document.createTextNode("mon titre audio")
     var textEdit = document.createElement("div")
     textEdit.appendChild(newTitle)
     document.body.insertBefore(textEdit, buttonsElement);
